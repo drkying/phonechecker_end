b4bdefef4ae206dffdfbe389fb90f0122433bb20
drkying <yatsunyin@gmail.com>
2023-12-15 15:43:51 +0800

    spring boot init

---
+++ src/main/java/com/drkying/phonechecker/PhonecheckerApplication.java
@@ -0,0 +1,13 @@
+package com.drkying.phonechecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhonecheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhonecheckerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/drkying/phonechecker/PhonecheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drkying.phonechecker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhonecheckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
